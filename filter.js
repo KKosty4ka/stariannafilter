8c2e7a8ca4ac0b83d64666c6a4ab25adbec9aa05
KKosty4ka <kostya.dis@outlook.com>
2024-02-02 19:23:26 +0700

    also it's nickname not username

--- filter.js
+++ filter.js
@@ -1,6 +1,6 @@
 // Currently a basic project only made by me and lime but you could add more to it by making a pull request.
 w.on("chatmod", function (e) {
-    if (e.message.includes('("Starianna")') || e.message.includes("🛑🛑🛑") || e.username.match(/(RUN|SPAM|THIS)[^]{0,}NOW/g)) {
+    if (e.message.includes('("Starianna")') || e.message.includes("🛑🛑🛑") || e.nickname.match(/(RUN|SPAM|THIS)[^]{0,}NOW/g)) {
         e.hide = true;
         w.chat.send(`/block ${e.id}`);
     }
