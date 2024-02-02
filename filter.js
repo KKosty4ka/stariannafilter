ac533cb1b732bcd879808d70bb16e8da80cb96d3
KKosty4ka <kostya.dis@outlook.com>
2024-02-02 19:22:41 +0700

    just block em

--- filter.js
+++ filter.js
@@ -1,8 +1,7 @@
 // Currently a basic project only made by me and lime but you could add more to it by making a pull request.
-let stariannaId;
 w.on("chatmod", function (e) {
-    if (e.message.includes('("Starianna")') || e.message.includes("🛑🛑🛑") || e.username.match(/(RUN|SPAM|THIS)[^]{0,}NOW/g) || stariannaId == e.id) {
+    if (e.message.includes('("Starianna")') || e.message.includes("🛑🛑🛑") || e.username.match(/(RUN|SPAM|THIS)[^]{0,}NOW/g)) {
         e.hide = true;
-        if (e.id != stariannaId) stariannaId = e.id;
+        w.chat.send(`/block ${e.id}`);
     }
 });
