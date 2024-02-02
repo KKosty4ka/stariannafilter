f09187a2b7eb2fbfc0f851bf49c6fabe55175a23
eggistaken2 <131176596+eggistaken2@users.noreply.github.com>
2024-02-02 09:12:52 -0300

    Reverted array to a single ID
    - Fixed comment
    - Reverted array to a single ID

--- filter.js
+++ filter.js
@@ -1,8 +1,8 @@
-// Currently a basic project only made by me but you could add more to it by making a pull request.
-let stariannaIds = [];
+// Currently a basic project only made by me and lime but you could add more to it by making a pull request.
+let stariannaId;
 w.on("chatmod", function (e) {
-    if (e.message.includes('("Starianna")') || e.message.includes("🛑🛑🛑") || e.username.match(/(RUN|SPAM|THIS)[^]{0,}NOW/g) || stariannaIds.includes(e.id)) {
+    if (e.message.includes('("Starianna")') || e.message.includes("🛑🛑🛑") || e.username.match(/(RUN|SPAM|THIS)[^]{0,}NOW/g) || stariannaId == e.id) {
         e.hide = true;
-        if (e.id && !stariannaIds.includes(e.id)) stariannaIds.push(e.id);
+        if (e.id != stariannaId) stariannaId = e.id;
     }
 });
